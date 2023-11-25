01becc67f7308bdcaa0a4144525fd9683e24a111
Esteban Espinoza Fallas <31984922+retornoaragond@users.noreply.github.com>
2023-11-25 13:15:05 -0600

    update 2

--- .nuxt/types/app.config.d.ts
+++ .nuxt/types/app.config.d.ts
@@ -5,7 +5,7 @@ import type { Defu } from 'defu'
 
 declare const inlineConfig = {
   "nuxt": {
-    "buildId": "0766b232-d42f-46aa-b9d9-2bc2dcac4595"
+    "buildId": "test"
   }
 }
 type ResolvedAppConfig = Defu<typeof inlineConfig, []>
